d65c9485e22330011adeb389b32decf82aaea07d
Ilikemebetter <pleum.41733@gmail.com>
2024-12-13 00:39:06 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,67 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string findChar(int age, int height, int bounty)
+{
+    string character;
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100)
+        {
+            character = "Chopper";
+        }
+        else if (height < 180)
+        {
+            character = "Usopp";
+        }
+        else
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000)
+            {
+                character = "Zoro";
+            }
+            else
+            {
+                character = "Sanji";
+            }
+        }
+    }
+    else
+    {
+        if (age <= 60)
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 500000000)
+            {
+                character = "Jinbe";
+            }
+            else
+            {
+                character = "Franky";
+            }
+        }
+        else
+        {
+            character = "Brook";
+        }
+    }
+
+    return character;
+}
+
+int main()
+{
+    int age=0, height=0, bounty=0;
+    cout << "Enter your age: ";
+    cin >> age;
+    string result;
+    result = findChar(age, height, bounty);
+    cout << "Your character = " << result;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,59 @@
-#include <iostream>
+#include <iostream> 
+#include <string>
 using namespace std;
 
-int main()
-{
-  char rank;
-  cout << "Input your rank: ";
-  cin >> rank;
-  if (rank = 'S'){
-    cout << "You have received Super Ultra Rare Unit!!!\n";
-    }if (rank = 'A'){
-    cout << "You have received 5 gems.\n";
-    }if (rank = 'B'){
-    cout << "You have received 1 gems.\n";
-    }if (rank = 'C'){ cout << "You have received 2000 coins.\n";
-    }if (rank ='D'){ cout << "You have received very KAK items.\n";
-    }else cout << " ";
-    return 0;
+string findChar(int age,int height,int bounty){
+    string character;
+    if (age > 60){
+        return character = "Brook";
+    }else if (bounty <= 500000000){
+        return character = "Franky";
+    }else{
+        return character = "Jinbe";
+    }
+    if (age <= 25){
+        if (height <= 100){
+            return character = "Chopper";
+        }else if(height > 100 && height <= 180){
+            return character = "Usopp";
+        }else if(bounty <= 1100000000){
+            return character = "Sanji";
+        }else{
+            return character = "Zoro";
+        }
+    }
 }
+    
+
+
+int main(){
+    int age, height, bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your height: ";
+    cin >> height;
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+
+    string findChar(int age,int height,int bounty);
+    
+    cout << "Your character = " << findChar(age,height,bounty);
+    return 0;
+
+}
+
+
+/*
+"Enter your age: "
+"Enter your height: "
+"Enter your bounty: "
+"Your character = "
+"Zoro"
+"Sanji"
+"Usopp"
+"Chopper"
+"Franky"
+"Brook"
+"Jinbe"
+*/
